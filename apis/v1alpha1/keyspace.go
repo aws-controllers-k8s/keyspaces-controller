0877f0c4e9dee3bc1ecb423869f1e88ff5417a4e
knottnt <trevor.knottnerus@gmail.com>
2026-09-17 08:56:13 -0700

    chore: remove aws-sdk-go (v1) dependency (#73)
    Issue #, if available: aws-controllers-k8s/community#2783
    
    Description of changes:
    Removes the transitive `github.com/aws/aws-sdk-go` (v1) dependency by
    bumping the referenced service controllers to releases whose
    `apis/v1alpha1` no longer force-import aws-sdk-go:
    
    - kms-controller -> v1.5.0
    
    After the bump, `go mod why -m github.com/aws/aws-sdk-go` reports
    "main module does not need module github.com/aws/aws-sdk-go" and the
    dependency is gone from the require block and go.sum, so it is no longer
    compiled into the controller binary. Generated code and ATTRIBUTION.md
    were regenerated to match. `go build ./...` and `go test ./...` pass.
    
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- apis/v1alpha1/keyspace.go
+++ apis/v1alpha1/keyspace.go
@@ -30,13 +30,12 @@ type KeyspaceSpec struct {
 	KeyspaceName *string `json:"keyspaceName"`
 	// The replication specification of the keyspace includes:
 	//
-	//   - replicationStrategy - the required value is SINGLE_REGION or MULTI_REGION.
+	//    * replicationStrategy - the required value is SINGLE_REGION or MULTI_REGION.
 	//
-	//   - regionList - if the replicationStrategy is MULTI_REGION, the regionList
-	//     requires the current Region and at least one additional Amazon Web Services
-	//     Region where the keyspace is going to be replicated in. The maximum number
-	//     of supported replication Regions including the current Region is six.
-	//
+	//    * regionList - if the replicationStrategy is MULTI_REGION, the regionList
+	//    requires the current Region and at least one additional Amazon Web Services
+	//    Region where the keyspace is going to be replicated in. The maximum number
+	//    of supported replication Regions including the current Region is six.
 	// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="Value is immutable once set"
 	ReplicationSpecification *ReplicationSpecification `json:"replicationSpecification,omitempty"`
 	// A list of key-value pair tags to be attached to the keyspace.
